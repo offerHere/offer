6e0ca33f4c219b91e570faf1fff499d0a504013c
niejunhong <niejunhong@wandoujia.com>
2015-07-04 10:09:18 +0800

    init offer project

---
+++ app/src/main/java/com/hxtech/offer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hxtech.offer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by niejunhong on 15-7-4.
+ */
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
