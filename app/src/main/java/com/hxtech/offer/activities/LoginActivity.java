6ef02adb2ec8288caa0523b6c31e6d2a0f22235b
kingsten <935178348@qq.com>
2015-07-26 12:43:40 +0800

    #Offer 增加QQ/微博 登录功能

--- app/src/main/java/com/hxtech/offer/activities/LoginActivity.java
+++ app/src/main/java/com/hxtech/offer/activities/LoginActivity.java
@@ -1,23 +1,23 @@
 package com.hxtech.offer.activities;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.hxtech.offer.R;
-import com.tencent.connect.UserInfo;
-import com.tencent.connect.auth.QQAuth;
-import com.tencent.open.utils.Util;
-import com.tencent.tauth.IUiListener;
-import com.tencent.tauth.Tencent;
-import com.tencent.tauth.UiError;
+
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
-import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qq.QQ;
 
 /**
  * Created by linjinquan01 on 2015/7/5.
@@ -26,109 +26,109 @@ import org.json.JSONObject;
 public class LoginActivity extends BaseActivity {
 
     public static final int QQ_NICK_NAME_MESSAGE = 0;
+
+
     @ViewById
-    ImageButton qqLoginButton;
+    Button loginQQ;
 
-    public static QQAuth mQQAuth;
+    @ViewById
+    Button loginWeibo;
 
-    private static Tencent mTencent;
+    private static final String QQ_APP_ID = "1104754270";
 
-    private UserInfo mQQUserInfo;
+    private static final String WEIBO_APP_KEY = "3699097253";
 
-    private static final String APP_ID = "1104754270";
+    private static final String WIEBO_REDIRECT_URL = "http://www.sina.com";
 
-    private static final String APP_KEY = "";
+    private static final String WEIBO_SCOPE =
+            "email,direct_messages_read,direct_messages_write,"
+                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+                    + "follow_app_official_microblog," + "invitation_write";
 
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == QQ_NICK_NAME_MESSAGE) {
-                JSONObject response = (JSONObject) msg.obj;
-                Log.e("reponse", "resepinse : " + response.toString());
-                if (response.has("nickname")) {
-                    try {
-                        Log.e("QQNIckName", "nickname : " + response.getString("nickname"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ShareSDK.initSDK(this);
     }
 
     @Override
     protected void onStart() {
-        mQQAuth = QQAuth.createInstance(APP_ID, getApplicationContext());
-        mTencent = Tencent.createInstance(APP_ID, getApplicationContext());
         super.onStart();
     }
 
-    public void onClickQQLogin(View v) {
-        Log.e("TARG", "QQ login click");
-        IUiListener listener = new BaseUiListener() {
+    public void onClickQQLogin(View view) {
+        login(QQ.NAME);
+    }
+
+    public void onClickWeiboLogin(View view) {
+        login(SinaWeibo.NAME);
+    }
+
+    public void logoutBtn(View view) {
+        logout(SinaWeibo.NAME);
+    }
+
+    private void login(String platformName) {
+        Platform platform = ShareSDK.getPlatform(platformName);
+        //sina.removeAccount();
+        if (platform.isValid()) {
+            String userId = platform.getDb().getUserId();
+            String name = platform.getDb().getUserName();
+            Toast.makeText(this, "name:" + name + ", id = " + userId, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        platform.setPlatformActionListener(new PlatformActionListener() {
+
             @Override
-            protected void doComplete(JSONObject values) {
-                Log.e("docomplete ", values.toString());
-                updateUseInfo();
+            public void onError(Platform platform, int arg1, Throwable arg2) {
+                // TODO Auto-generated method stub
+                LoginActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
-        };
-        mQQAuth.login(this, "all", listener);
-        mTencent.login(this, "all", listener);
+
+            @Override
+            public void onComplete(final Platform platform, int arg1, HashMap<String, Object> arg2) {
+                // TODO Auto-generated method stub
+                LoginActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "onComplete", Toast.LENGTH_SHORT).show();
+                        String userId = platform.getDb().getUserId();
+                        String name = platform.getDb().getUserName();
+                        Toast.makeText(LoginActivity.this, "name:" + name + ", id = " + userId, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int arg1) {
+                // TODO Auto-generated method stub
+                LoginActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "onCancel", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+        //sina.authorize();
+        platform.SSOSetting(false);
+        platform.showUser(null);
     }
 
-    private void updateUseInfo() {
-        Log.e("TAG", "login success,update useInfo : " + mQQAuth.isSessionValid());
-        if (mQQAuth != null) {
-            IUiListener listener = new IUiListener() {
-                @Override
-                public void onComplete(Object response) {
-                    Message msg = new Message();
-                    msg.obj = response;
-                    msg.what = QQ_NICK_NAME_MESSAGE;
-                    handler.sendMessage(msg);
-                }
 
-                @Override
-                public void onError(UiError uiError) {
-
-                }
-
-                @Override
-                public void onCancel() {
-
-                }
-            };
-            mQQUserInfo = new UserInfo(this, mQQAuth.getQQToken());
-            mQQUserInfo.getUserInfo(listener);
-        }
+    private void logout(String platform) {
+        ShareSDK.getPlatform(this, platform).removeAccount();
     }
 
-    private class BaseUiListener implements IUiListener {
-
-        @Override
-        public void onComplete(Object response) {
-            Util.showAlert(LoginActivity.this, response.toString(),
-                    "登录成功");
-            doComplete((JSONObject) response);
-        }
-
-        protected void doComplete(JSONObject values) {
-
-        }
-
-        @Override
-        public void onError(UiError e) {
-            Util.showAlert(LoginActivity.this, "onError: " + e.errorDetail, "test");
-        }
-
-        @Override
-        public void onCancel() {
-            Util.showAlert(LoginActivity.this, "onCancel: ", "test");
-        }
-    }
 }
